d53ee0eff96e580ab4ed9871c5c4251eebf13fbc
Osman Yasir <Osmanyasir.ozyilmaz@iaau.edu.kg>
2020-11-08 13:17:28 +0600

    update

--- src/sample/Student.java
+++ src/sample/Student.java
@@ -1,47 +1,21 @@
 package sample;
 
-public class Student {
-    String FirstName;
-    String LastName;
+
+
+
+public class Student extends Person {
+
     String Group;
-    int BirthYear;
 
     public Student(String firstName, String lastName, String group, int birthYear) {
-        FirstName = firstName;
-        LastName = lastName;
+        super(firstName,lastName,birthYear);
         Group = group;
-        BirthYear = birthYear;
     }
-
-    public String getFirstName() {
-        return FirstName;
-    }
-
-    public void setFirstName(String firstName) {
-        FirstName = firstName;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        LastName = lastName;
-    }
-
     public String getGroup() {
         return Group;
     }
 
     public void setGroup(String group) {
         Group = group;
-    }
-
-    public int getBirthYear() {
-        return BirthYear;
-    }
-
-    public void setBirthYear(int birthYear) {
-        BirthYear = birthYear;
     }
 }
