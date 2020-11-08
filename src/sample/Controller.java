caae2c27f349d8890634310778e2d13aa34dc21f
Osman Yasir <Osmanyasir.ozyilmaz@iaau.edu.kg>
2020-11-08 11:58:44 +0600

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,6 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,34 +18,52 @@ public class Controller {
     public TextField savetf;
     public TextField opentf;
     public Label textlbl;
+    public Label lbl2;
+    public Button clearbtn;
 
 
     public void handleButtonAction(ActionEvent actionEvent) {
         if (actionEvent.getSource() == addbtn) {
-            String tmp = textlbl.getText();
-            textlbl.setText(tmp + getalltext());
+            addText();
 
         }
         else if (actionEvent.getSource() == Savebtn) {
-            Savetofile();
+            saveToFile();
         }
         else if (actionEvent.getSource() == Openbtn) {
-            ReadFile();
+            readFile();
+        }
+        else if (actionEvent.getSource() == clearbtn) {
+            clear();
         }
     }
 
-    private String ReadFile() {
+    public String addText() {
+        String tmp = textlbl.getText();
+        textlbl.setText(tmp + getalltext());
+        System.out.println("Added text");
+        return "Added text";
+    }
+
+    public String clear() {
+        textlbl.setText("");
+        System.out.println("Cleared text");
+        return "Cleared text";
+    }
+
+    public String readFile() {
         try {
 
             String text = opentf.getText();
             String all = Files.readString(Path.of(text));
             textlbl.setText(all);
+            lbl2.setText("File Opened");
             System.out.println(all);
 
             return "File read";
         }
         catch (Exception e){
-            textlbl.setText("no such file or you forgot .txt");
+            lbl2.setText("no such file or you forgot .txt");
             return "File not read";
         }
 
@@ -58,19 +75,20 @@ public class Controller {
         return text;
     }
 
-    private String Savetofile() {
+    public String saveToFile() {
         try {
             String name = savetf.getText();
-            FileWriter writer = new FileWriter(name, true);
+            FileWriter writer = new FileWriter(name);
             writer.write(textlbl.getText());
             writer.close();
+            lbl2.setText("Success");
             System.out.println("Success");
             return "Success";
         }
 
         catch (Exception e){
             System.out.println("gg");
-            textlbl.setText("Something went wrong");
+            lbl2.setText("Something went wrong");
             return "Failed";
         }
 
